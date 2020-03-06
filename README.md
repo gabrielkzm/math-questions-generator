293b75f10536a91c9e02d6f797d6f0e96a9def6c
Gabriel Koh <gabriel.koh.2016@sis.smu.edu.sg>
2020-03-06 23:22:57 +0800

    First commit + added feature on time based questions for primary level

---
+++ README.md
@@ -0,0 +1,51 @@
+# Math Question Generator
+
+**Match Question Generator** is a python based backend engine that helps tuition centres and teachers to generate math questions for students of different education levels - Primary, Secondary, Junior College. This project is currently in its infancy stage and is focused algorithms and business logic behind generation of questions. Future implmentation may involve a frontend interface to aid with question generation.
+
+Should you wish to find out more, please contact me at my email (gabrielkoh@live.com) or via [linkedin](https://www.linkedin.com/in/gabrielkohzm).
+
+---
+
+## 1. Key Features:
+
+### **1.1. Primary**
+- Time Based Questions
+  
+### **1.2. Secondary**
+
+### **1.3.  Junior College**
+
+---
+
+## 2. Pre-requisites
+
+- Python
+- pip
+
+---
+
+## 3. Getting Started
+
+In order to start the application, please follow the subsequent steps:
+
+1. cd into /math-questions-generator
+
+2. Install all relevant dependencies
+   
+```bash
+pip install pycountry
+pip install pytz
+pip install python-docx
+```
+
+3. Run appropriate python file based on the type of questions you would want to generate - based on legend in Section 1
+
+   - For example, if you want to generate time-based questions, run 
+
+            python time-based-questions.py
+
+4. View updated word document to see list of questions generated. For example, for time base questions, see TimeBasedQuestions.docx
+
+---
+
+*Last Updated: 6 March 2020*
---
+++ time-based-questions.py
@@ -0,0 +1,87 @@
+import random, datetime, pytz, pycountry
+
+from docx import Document
+
+def formatDocument():
+    document = Document()
+    document.add_heading('Time Based Questions', 0)
+    p = document.add_paragraph()
+    p.add_run('This document contains time based questions.').bold = True
+
+    return document
+
+def getCountries():
+    countryOrigin = countries[random.randrange(0,len(countries))]
+    countryDest = countries[random.randrange(0,len(countries))]
+    while countryOrigin == countryDest:
+        countryOrigin = countries[random.randrange(0,len(countries))]
+        countryDest = countries[random.randrange(0,len(countries))]
+
+    return countryOrigin, countryDest
+
+def getCountriesAndTimezones():
+    countryOrigin, countryDest = getCountries()
+    while True:
+        try:
+            tzOrigin = pytz.country_timezones[countryOrigin.alpha_2][0]
+            tzDestination = pytz.country_timezones[countryDest.alpha_2][0]
+            break
+        except:
+            countryOrigin, countryDest = getCountries()
+            pass
+
+    return countryOrigin, countryDest, tzOrigin, tzDestination
+
+def getHour():
+    return random.randrange(0,24)
+
+def getMinute():
+    return random.randrange(0,60)
+
+def getFormatedDateTime(datetime):
+    return datetime.strftime("%d/%m/%Y, %H:%M")
+
+### Main Method Starts Here
+
+numberOfQuestions = 100
+countries = list(pycountry.countries)
+document = formatDocument()
+
+for i in range(numberOfQuestions):
+    countryOrigin, countryDest, tzOrigin, tzDestination = getCountriesAndTimezones()
+
+    hourOrigin = getHour()
+    minuteOrigin = getMinute()
+    utcOrigin = datetime.datetime(2020,3,4,hourOrigin, minuteOrigin, 0, 0, tzinfo = pytz.utc)
+
+    dateTimeOrigin = utcOrigin.astimezone(pytz.timezone(tzOrigin))
+    formattedOrigin = getFormatedDateTime(dateTimeOrigin)
+
+    dateTimeDestination = utcOrigin.astimezone(pytz.timezone(tzDestination))
+    formattedDestination = getFormatedDateTime(dateTimeDestination)
+
+    randomHourOrigin = getHour()
+    minuteOrigin = getMinute()
+    randomUTCOrigin = datetime.datetime(2020,3,4,randomHourOrigin, minuteOrigin, 0, 0, tzinfo= pytz.utc)
+    randomOriginDateTime = randomUTCOrigin.astimezone(pytz.timezone(tzOrigin))
+    randomFormattedOrigin = getFormatedDateTime(randomOriginDateTime)
+
+    durationHours = getHour()
+    durationMinutes = getMinute()
+
+    sectionOne = f'Question {i+1}:'
+    sectionTwo = f'When the date and ime in {countryOrigin.name} is {formattedOrigin} respectively, ' \
+        + f'the date and time in {countryDest.name} is {formattedDestination} respectively.'
+    sectionThree = f' A plane leaves {countryOrigin.name} at {randomFormattedOrigin} local date and time and takes {durationHours}' \
+        + f' hours and {durationMinutes} minutes to fly to {countryDest.name}.'
+    sectionFour = f'What is the date and time in {countryDest.name} when it arrives?'
+    answer = getFormatedDateTime((randomUTCOrigin + datetime.timedelta(0,0,0,0, durationMinutes, durationHours,0)).astimezone(pytz.timezone(tzDestination)))
+    sectionFive = f'Answer: {answer}'
+
+    document.add_paragraph(sectionOne)
+    p = document.add_paragraph(sectionTwo)
+    p.add_run(sectionThree)
+    document.add_paragraph(sectionFour)
+    document.add_paragraph(sectionFive)
+
+document.save('test.docx')
